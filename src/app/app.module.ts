0db1af2743cde4a183e99f99a9f00de97ee96517
vilayan <valuator27@gmail.com>
2017-07-01 11:15:18 +0700

    install libary material flexlayout firebase2 firebase2notification + config firebase2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,68 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+
+/***Angular Libary*/
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+    MdButtonModule, MdCardModule, MdDialogModule, MdGridListModule, MdIconModule, MdIconRegistry, MdInputModule,
+    MdMenuModule,
+    MdProgressSpinnerModule,
+    MdSidenavModule, MdSlideToggleModule,
+    MdToolbarModule, MdTooltipModule
+} from '@angular/material';
+import 'hammerjs';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+/*** Angular Firebase*/
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+
+/*** Angular notifications*/
+import {PushNotificationsModule, SimpleNotificationsModule} from 'angular2-notifications';
+
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDKfhalS9iXAH1Lqr_Z7HcoYF0mHHmHfd0',
+    authDomain: 'dbook-8d9fa.firebaseapp.com/',
+    databaseURL: 'https://dbook-8d9fa.firebaseapp.com/',
+    projectId: 'dbook-8d9fa',
+    storageBucket: 'dbook-8d9fa.appspot.com',
+    messagingSenderId: '951635947188'
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MdButtonModule,
+        MdToolbarModule,
+        MdMenuModule,
+        MdCardModule,
+        MdInputModule,
+        MdIconModule,
+        MdProgressSpinnerModule,
+        MdTooltipModule,
+        MdGridListModule,
+        MdSidenavModule,
+        MdDialogModule,
+        MdSlideToggleModule,
+        FlexLayoutModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireDatabaseModule,
+        AngularFireAuthModule,
+        PushNotificationsModule,
+        SimpleNotificationsModule
+
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
