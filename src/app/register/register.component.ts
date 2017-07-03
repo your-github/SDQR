849d9e0a99ff2b5c40f12162e09d8bae9b11b10c
vilayan <valuator27@gmail.com>
2017-07-03 19:59:24 +0700

    Add Form Register in to register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  propic() {
+    document.getElementById('propic').click();
+  }
 }
