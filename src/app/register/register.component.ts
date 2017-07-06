8b6cb9d1d5bfffc1fbbb72bb61523d03e0f08f28
vilayan <valuator27@gmail.com>
2017-07-06 11:27:50 +0700

    add toolbar into login and register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,7 +7,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
@@ -15,5 +15,9 @@ export class RegisterComponent implements OnInit {
 
   propic() {
     document.getElementById('propic').click();
+  }
+
+  goHome() {
+    this.router.navigate(['home']);
   }
 }
