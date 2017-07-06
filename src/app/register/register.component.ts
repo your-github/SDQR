b884f90099a3f5463dc78d914ea723a0da1c3f7a
Youathor Yiaveu <yourthor17121996@gmail.com>
2017-07-06 22:29:04 +0700

    install ngx-validators and fix Register validator

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 import {UserService} from '../service/user.service';
+import {PasswordValidators} from 'ngx-validators';
 
 @Component({
   selector: 'app-register',
@@ -19,16 +20,18 @@ export class RegisterComponent implements OnInit {
 
   constructor(private router: Router, private formBuild: FormBuilder, private userService: UserService) {
     this.fRegister = formBuild.group({
-      fname: ['', Validators.required],
-      lname: ['', Validators.required],
-      email: [
+      'fname': ['', Validators.required],
+      'lname': ['', Validators.required],
+      'email': [
         '',
-        Validators.required,
-        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+        [
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+        ]
       ],
-      password: ['', Validators.required],
-      confirm: ['', [Validators.required]]
-    })
+      'password': ['', Validators.required],
+      'confirm': ['', Validators.required]
+    }, PasswordValidators.mismatchedPasswords('password', 'confirm'));
   }
 
   ngOnInit() {
@@ -52,12 +55,15 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    const user = this.fRegister.value;
-    user.pic = this.userpic64;
-    this.userService.register(user).then(success => {
-      console.log(success);
-    }).catch(error => {
-      console.log(error);
-    })
+    if (this.fRegister.valid) {
+      const user = this.fRegister.value;
+      user.pic = this.userpic64;
+      console.log(user);
+    }
+    /*this.userService.register(user).then(success => {
+     console.log(success);
+     }).catch(error => {
+     console.log(error);
+     });*/
   }
 }
